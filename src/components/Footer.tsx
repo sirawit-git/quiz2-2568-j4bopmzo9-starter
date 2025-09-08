7bfec767f027ebe64f22546e9109032ccf71c16a
Champ <champsrw49@gmail.com>
2025-09-08 11:10:34 +0700

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,5 @@
 import { Text, Group } from "@mantine/core";
+import { type FooterProps } from "../libs/Footer";
 export default function Footer({ year, fullName, studentId, courseName }: FooterProps) {
   return (
     <Group p="md" justify="center">
